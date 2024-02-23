a274b95f06a49fffb2eadae8f238f465a76354d8
Steven Eubank <eubank.steven88@gmail.com>
2024-02-23 16:58:45 +0100

    remove clients from edge

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -4,18 +4,9 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { SupabaseClient } from "@supabase/supabase-js";
-import { SupabaseIntegration } from "@supabase/sentry-js-integration";
 
 Sentry.init({
   dsn: "https://48729b85799679d2563b685a53c96fc9@o673219.ingest.sentry.io/4506789483773952",
-  integrations: [
-    new SupabaseIntegration(SupabaseClient, {
-      tracing: true,
-      breadcrumbs: true,
-      errors: true,
-    }),
-  ],
   tracesSampleRate: 1,
   debug: true,
 });
