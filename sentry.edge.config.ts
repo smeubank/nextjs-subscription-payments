b7455a186ef0c48070871184a7049a833828b238
Steven Eubank <eubank.steven88@gmail.com>
2024-02-26 14:47:23 +0100

    add wintercg and supabase integration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -4,9 +4,24 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { SupabaseClient } from '@supabase/supabase-js'
+import { SupabaseIntegration } from '@supabase/sentry-js-integration'
 
 Sentry.init({
   dsn: "https://48729b85799679d2563b685a53c96fc9@o673219.ingest.sentry.io/4506789483773952",
   tracesSampleRate: 1,
   debug: true,
+  integrations: [
+    new SupabaseIntegration(SupabaseClient, {
+      tracing: true,
+      breadcrumbs: true,
+      errors: true,
+    }),
+    new Sentry.Integrations.WinterCGFetch({
+      breadcrumbs: true,
+      shouldCreateSpanForRequest: (url) => {
+        return !url.startsWith(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest`)
+      },
+    }),
+  ],
 });
